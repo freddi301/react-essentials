071dc4b69b9d2c938c6c6e3ffed8b4eacf7c3cf2
Frederik Batuna <gobi301@gmail.com>
2023-07-14 16:50:56 +0200

    add: default query revalidation

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,7 @@
 import React from "react";
 
-// TODO apply @tanstack/react-query cache invalidation (see page docs)
-// TODO retry queries on error (see @tanstack/react-query)
-// TODO let user track mutations
+// TODO structural sharing
+// TODO let user track mutations more easily (just returning mutation promise now)
 
 type Resource<Variables, Data> = {
   read(variables: Variables): Data;
@@ -11,16 +10,41 @@ type Resource<Variables, Data> = {
   invalidateExact(variables: Variables): void;
   invalidatePartial(variables: RecursivelyPartial<Variables>): void;
   subscribe(variables: Variables, listener: Listener): Unsubscribe;
-  useData(variables: Variables): Data;
+  useData(
+    variables: Variables,
+    options?: { revalidateOnMount?: boolean }
+  ): Data;
 };
 
 type Listener = () => void;
 type Unsubscribe = () => void;
 
 export function createResource<Variables, Data>(
-  resolver: (variables: Variables) => Promise<Data>
+  resolver: (variables: Variables) => Promise<Data>,
+  {
+    shouldRetry = ({ retries }) => {
+      if (retries > 3) return false;
+      return 1000 * Math.pow(retries, 2);
+    },
+    revalidateOnFocus = true,
+    revalidateOnConnect = true,
+    revalidateAfter = 5 * 60 * 1000,
+  }: {
+    shouldRetry?(_: { retries: number; error: unknown }): number | false;
+    revalidateOnFocus?: boolean;
+    revalidateOnConnect?: boolean;
+    revalidateAfter?: number;
+  } = {}
 ): Resource<Variables, Data> {
-  type Entry = { data: Data } | { promise: Promise<Data> };
+  type Entry =
+    | { data: Data; isValid: true; revalidateAfterTimeoutId: number }
+    | {
+        data: Data;
+        isValid: false;
+        promise?: Promise<Data>;
+        revalidateAfterTimeoutId: number;
+      }
+    | { promise: Promise<Data> };
   const cache = new Map<Variables, Entry>();
   const findEntry = (
     variables: Variables
@@ -32,25 +56,61 @@ export function createResource<Variables, Data>(
     }
   };
   const subscriptions = new Set<{ variables: Variables; listener: Listener }>();
-  return {
+  const tryResolve = async (variables: Variables): Promise<Data> => {
+    let retries = 1;
+    let error;
+    while (shouldRetry({ retries, error })) {
+      try {
+        return await resolver(variables);
+      } catch (e) {
+        error = e;
+        retries++;
+      }
+    }
+    throw error;
+  };
+  const udpateEntry = (variables: Variables) => (data: Data) => {
+    const found = findEntry(variables);
+    if (found && "revalidateAfterTimeoutId" in found.entry) {
+      window.clearTimeout(found.entry.revalidateAfterTimeoutId);
+    }
+    const revalidateAfterTimeoutId = window.setTimeout(() => {
+      resource.invalidateExact(variables);
+    }, revalidateAfter);
+    cache.set(variables, { isValid: true, data, revalidateAfterTimeoutId });
+    subscriptions.forEach((subscription) => {
+      if (deepIsEqual(subscription.variables, variables)) {
+        subscription.listener();
+      }
+    });
+  };
+  const resource: Resource<Variables, Data> = {
     read(variables) {
       const found = findEntry(variables);
       if (!found) {
-        const promise = resolver(variables);
+        const promise = tryResolve(variables);
         cache.set(variables, { promise });
-        promise.then((data) => {
-          cache.set(variables, { data });
-        });
+        promise.then(udpateEntry(variables));
         throw promise;
       } else {
+        if ("isValid" in found.entry && !found.entry.isValid) {
+          const promise = tryResolve(variables);
+          cache.set(variables, {
+            data: found.entry.data,
+            isValid: false,
+            promise,
+            revalidateAfterTimeoutId: found.entry.revalidateAfterTimeoutId,
+          });
+          promise.then(udpateEntry(variables));
+        }
         if ("data" in found.entry) return found.entry.data;
         else throw found.entry.promise;
       }
     },
     invalidate(criteria) {
-      for (const variables of cache.keys()) {
+      for (const [variables, entry] of cache.entries()) {
         if (criteria(variables)) {
-          cache.delete(variables);
+          if ("isValid" in entry) entry.isValid = false;
           subscriptions.forEach((subscription) => {
             if (deepIsEqual(subscription.variables, variables)) {
               subscription.listener();
@@ -60,33 +120,43 @@ export function createResource<Variables, Data>(
       }
     },
     invalidateAll() {
-      this.invalidate(() => true);
+      resource.invalidate(() => true);
     },
     invalidateExact(variables) {
-      this.invalidate((other) => deepIsEqual(variables, other));
+      resource.invalidate((other) => deepIsEqual(variables, other));
     },
     invalidatePartial(variables) {
-      this.invalidate((other) => partialDeepEqual(variables, other));
+      resource.invalidate((other) => partialDeepEqual(variables, other));
     },
     subscribe(variables, listener) {
       const subscription = { variables, listener };
       subscriptions.add(subscription);
       return () => subscriptions.delete(subscription);
     },
-    useData(variables) {
+    useData(variables, { revalidateOnMount = true } = {}) {
       const [, forceUpdate] = React.useState(0);
-      React.useEffect(
-        () =>
-          this.subscribe(variables, () => forceUpdate((count) => count + 1)),
-        [variables]
-      );
-      return this.read(variables);
+      React.useEffect(() => {
+        if (revalidateOnMount) {
+          resource.invalidateExact(variables);
+        }
+        return resource.subscribe(variables, () =>
+          forceUpdate((count) => count + 1)
+        );
+      }, [variables, revalidateOnMount]);
+      return resource.read(variables);
     },
   };
+  if (revalidateOnFocus) {
+    window.addEventListener("focus", resource.invalidateAll);
+  }
+  if (revalidateOnConnect) {
+    window.addEventListener("online", resource.invalidateAll);
+  }
+  return resource;
 }
 
 type Mutation<Variables, Data> = {
-  mutate(variables: Variables): void;
+  mutate(variables: Variables): Promise<Data>;
 };
 
 export function createMutation<Variables, Data>(
@@ -100,15 +170,15 @@ export function createMutation<Variables, Data>(
   }
 ): Mutation<Variables, Data> {
   return {
-    mutate(variables) {
-      performer(variables).then(
-        (data) => {
-          onSuccess?.({ variables, data });
-        },
-        (error) => {
-          onError?.({ variables, error });
-        }
-      );
+    async mutate(variables) {
+      try {
+        const data = await performer(variables);
+        onSuccess?.({ variables, data });
+        return data;
+      } catch (error) {
+        onError?.({ variables, error });
+        throw error;
+      }
     },
   };
 }
